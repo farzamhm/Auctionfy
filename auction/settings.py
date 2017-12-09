324acb0976852570c5d2bd2d6cc4ea934015b864
zohreh Hosseinzadeh <zhosseinzadeh.hanza@yahoo.com>
2017-12-09 16:15:49 -0500

    Ready to go to heroku

--- auction/settings.py
+++ auction/settings.py
@@ -81,20 +81,20 @@ WSGI_APPLICATION = 'auction.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-SECRET_KEY =  os.environ.get('SECRET_KEY')
-
-DEBUG = False
-
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
+# SECRET_KEY =  os.environ.get('SECRET_KEY')
+
+# DEBUG = False
+
+# DATABASES = {
+#     'default': dj_database_url.config()
+# }
 
 # DATABASES = {
 #     "default": {
