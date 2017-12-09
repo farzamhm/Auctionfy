fdd790b8db8e1c6c50086ebf6fe35f7f418ecd2d
zohreh Hosseinzadeh <zhosseinzadeh.hanza@yahoo.com>
2017-12-09 14:39:40 -0500

    adding Procfile and changing statics'

--- auction/settings.py
+++ auction/settings.py
@@ -87,7 +87,6 @@ WSGI_APPLICATION = 'auction.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 DATABASES = {
     'default': dj_database_url.config(
