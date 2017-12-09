3b94db7b4bb342cd4600d30a23d5ab2cd75aeee8
zohreh Hosseinzadeh <zhosseinzadeh.hanza@yahoo.com>
2017-12-09 01:48:02 -0500

    adding Procfile and changed static

--- auction/settings.py
+++ auction/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -88,9 +87,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-DATABASES = {
-    'default': dj_database_url.config(default="postgres:///auction", conn_max_age=500)
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -165,7 +162,7 @@ CHANNEL_LAYERS = {
         # This example app uses the Redis channel layer implementation asgi_redis
         "BACKEND": "asgi_redis.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            "hosts": [(redis_host, 6379)],
         },
        "ROUTING": "auction.routing.channel_routing", # We will create it in a moment
     },
